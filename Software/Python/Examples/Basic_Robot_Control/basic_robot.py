75bce52787c45dc4429e49c7c458682572f79f6b
dussotro <romain.dussot@ensta-bretagne.org>
2019-04-22 09:59:13 +0200

    python2 to 3

--- Software/Python/Examples/Basic_Robot_Control/basic_robot.py
+++ Software/Python/Examples/Basic_Robot_Control/basic_robot.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 #############################################################################################################                                                                  
 # Basic example for controlling the GoPiGo using the Keyboard
 # Controls:
@@ -36,30 +36,41 @@ along with this program.  If not, see <http://www.gnu.org/licenses/gpl-3.0.txt>.
 #
 ##############################################################################################################
 
-from gopigo import *	#Has the basic functions for controlling the GoPiGo Robot
-import sys	#Used for closing the running program
-print "This is a basic example for the GoPiGo Robot control"
-print "Press:\n\tw: Move GoPiGo Robot forward\n\ta: Turn GoPiGo Robot left\n\td: Turn GoPiGo Robot right\n\ts: Move GoPiGo Robot backward\n\tt: Increase speed\n\tg: Decrease speed\n\tx: Stop GoPiGo Robot\n\tz: Exit\n"
+import gopigo as gpg 	#Has the basic functions for controlling the GoPiGo Robot
+import sys				#Used for closing the running program
+import time
+
+print("This is a basic example for the GoPiGo Robot control")
+print("Press:\n\tw: Move GoPiGo Robot forward 		\
+			 \n\ta: Turn GoPiGo Robot left 		 	\
+			 \n\td: Turn GoPiGo Robot right			\
+			 \n\ts: Move GoPiGo Robot backward		\
+			 \n\tt: Increase speed					\
+			 \n\tg: Decrease speed					\
+			 \n\tx: Stop GoPiGo Robot				\
+			 \n\tz: Exit							\
+			 \n")									
+
 while True:
-	print "Enter the Command:",
-	a=raw_input()	# Fetch the input from the terminal
+	print("Enter the Command:")
+	a=input()	# Fetch the input from the terminal
 	if a=='w':
-		fwd()	# Move forward
+		gpg.fwd()	# Move forward
 	elif a=='a':
-		left()	# Turn left
+		gpg.left()	# Turn left
 	elif a=='d':
-		right()	# Turn Right
+		gpg.right()	# Turn Right
 	elif a=='s':
-		bwd()	# Move back
+		gpg.bwd()	# Move back
 	elif a=='x':
-		stop()	# Stop
+		gpg.stop()	# Stop
 	elif a=='t':
-		increase_speed()	# Increase speed
+		gpg.increase_speed()	# Increase speed
 	elif a=='g':
-		decrease_speed()	# Decrease speed
+		gpg.decrease_speed()	# Decrease speed
 	elif a=='z':
-		print "Exiting"		# Exit
+		print("Exiting")		# Exit
 		sys.exit()
 	else:
-		print "Wrong Command, Please Enter Again"
+		print("Wrong Command, Please Enter Again")
 	time.sleep(.1)
